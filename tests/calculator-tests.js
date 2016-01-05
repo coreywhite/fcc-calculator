0f186c4ec9806c4a3ab289b404e55a3f9dbfacba
Corey White <white.corey.a@gmail.com>
2016-01-04 20:07:06 -0500

    Reorganized tests with nested modules

--- tests/calculator-tests.js
+++ tests/calculator-tests.js
@@ -9,52 +9,92 @@ QUnit.test("Calculator Object Creation", function( assert ) {
 
 //Set up a series of tests to demonstrate calculator behavior in response to
 //sequences of command inputs.
-QUnit.module("Calculator Input Sequences", {
-  beforeEach: function() {
+QUnit.module("Calculator Input Sequences", function(hooks) {
+
+  hooks.beforeEach(function(assert) {
     display.text = "";
     this.calc = new Calculator(8, function(displayString) {
       display.text = displayString;
     });
-  }});
+  });
 
-//Demonstrate restriction on entering multiple zeros
-QUnit.test("Zero Entry Functionality", function( assert ) {
-  this.calc.Command(0);
-  assert.strictEqual(display.text, "0", "Enter 0");
-  this.calc.Command(0);
-  assert.strictEqual(display.text, "0", "Only one zero should be displayed");
-});
+  QUnit.module("Numeric Entry", function(hooks) {
 
-//Demonstrate addition
-QUnit.test("Test 1 + 2 =", function( assert ) {
-  assert.expect(4);
-  this.calc.Command(1);
-  assert.strictEqual(display.text, "1", "Enter 1, display 1");
-  this.calc.Command("+");
-  assert.strictEqual(display.text, "1", "Enter +, display 1");
-  this.calc.Command(2);
-  assert.strictEqual(display.text, "2", "Enter 2, display 2");
-  this.calc.Command("=");
-  assert.strictEqual(display.text, "3", "Enter =, display 3");
-});
+    QUnit.test("Basic Numeric Entry", function( assert ) {
+      assert.expect(1);
+      this.calc.Command(1);
+      assert.strictEqual(display.text, "1", "Enter 1, display 1");
+    });
 
-//Demonstrate simple operator chaining and two digit display
-QUnit.test("Test 1 + 2 + 3 + 4 =", function( assert ) {
-  assert.expect(8);
-  this.calc.Command(1);
-  assert.strictEqual(display.text, "1", "Enter 1, display 1");
-  this.calc.Command("+");
-  assert.strictEqual(display.text, "1", "Enter +, display 1");
-  this.calc.Command(2);
-  assert.strictEqual(display.text, "2", "Enter 2, display 2");
-  this.calc.Command("+");
-  assert.strictEqual(display.text, "3", "Enter +, display 3");
-  this.calc.Command(3);
-  assert.strictEqual(display.text, "3", "Enter 3, display 3");
-  this.calc.Command("+");
-  assert.strictEqual(display.text, "6", "Enter +, display 6");
-  this.calc.Command(4);
-  assert.strictEqual(display.text, "4", "Enter 4, display 4");
-  this.calc.Command("=");
-  assert.strictEqual(display.text, "10", "Enter =, display 10");
+    QUnit.test("Multiple Digit Entry", function( assert ) {
+      assert.expect(3);
+      this.calc.Command(1);
+      assert.strictEqual(display.text, "1", "Enter 1, display 1");
+      this.calc.Command(2);
+      assert.strictEqual(display.text, "12", "Enter 2, display 12");
+      this.calc.Command(3);
+      assert.strictEqual(display.text, "123", "Enter 3, display 123");
+    });
+
+    QUnit.test("Maximum digit restriction", function( assert ) {
+      assert.expect(1);
+      this.calc.Command(1);
+      this.calc.Command(2);
+      this.calc.Command(3);
+      this.calc.Command(4);
+      this.calc.Command(5);
+      this.calc.Command(6);
+      this.calc.Command(7);
+      this.calc.Command(8);
+      this.calc.Command(9);
+      assert.strictEqual(display.text, "12345678", "Display first 8 digits");
+    });
+
+    QUnit.test("Multiple Zero Entry Restriction", function( assert ) {
+      assert.expect(2);
+      this.calc.Command(0);
+      assert.strictEqual(display.text, "0", "Enter 0, Display 0");
+      this.calc.Command(0);
+      assert.strictEqual(display.text, "0", "Only one zero should be displayed");
+    });
+  });
+
+  QUnit.module("Operators", function(hooks) {
+
+    //Demonstrate addition
+    QUnit.test("Test 1 + 2 =", function( assert ) {
+      assert.expect(4);
+      this.calc.Command(1);
+      assert.strictEqual(display.text, "1", "Enter 1, display 1");
+      this.calc.Command("+");
+      assert.strictEqual(display.text, "1", "Enter +, display 1");
+      this.calc.Command(2);
+      assert.strictEqual(display.text, "2", "Enter 2, display 2");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "3", "Enter =, display 3");
+    });
+  });
+
+  QUnit.module("Binary Operator Chains", function(hooks) {
+    //Demonstrate simple operator chaining and two digit display
+    QUnit.test("Test 1 + 2 + 3 + 4 =", function( assert ) {
+      assert.expect(8);
+      this.calc.Command(1);
+      assert.strictEqual(display.text, "1", "Enter 1, display 1");
+      this.calc.Command("+");
+      assert.strictEqual(display.text, "1", "Enter +, display 1");
+      this.calc.Command(2);
+      assert.strictEqual(display.text, "2", "Enter 2, display 2");
+      this.calc.Command("+");
+      assert.strictEqual(display.text, "3", "Enter +, display 3");
+      this.calc.Command(3);
+      assert.strictEqual(display.text, "3", "Enter 3, display 3");
+      this.calc.Command("+");
+      assert.strictEqual(display.text, "6", "Enter +, display 6");
+      this.calc.Command(4);
+      assert.strictEqual(display.text, "4", "Enter 4, display 4");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "10", "Enter =, display 10");
+    });
+  });
 });

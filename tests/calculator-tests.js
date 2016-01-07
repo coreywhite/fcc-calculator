70f1b8477985fe40e090226b16e24898e60503de
Corey White <white.corey.a@gmail.com>
2016-01-06 20:05:01 -0500

    Added tests for arithmetic operators

--- tests/calculator-tests.js
+++ tests/calculator-tests.js
@@ -28,11 +28,11 @@ QUnit.module("Calculator Input Sequences", function(hooks) {
 
     QUnit.test("Multiple Digit Entry", function(assert) {
       assert.expect(3);
-      this.calc.Command(1);
+      this.calc.Command("1");
       assert.strictEqual(display.text, "1", "Enter 1, display 1");
-      this.calc.Command(2);
+      this.calc.Command("2");
       assert.strictEqual(display.text, "12", "Enter 2, display 12");
-      this.calc.Command(3);
+      this.calc.Command("3");
       assert.strictEqual(display.text, "123", "Enter 3, display 123");
     });
 
@@ -124,10 +124,10 @@ QUnit.module("Calculator Input Sequences", function(hooks) {
 
   });
 
+  //Demonstrate individual arithmetic operators
   QUnit.module("Operators", function(hooks) {
 
-    //Demonstrate addition
-    QUnit.test("Test 1 + 2 =", function(assert) {
+    QUnit.test("Addition", function(assert) {
       assert.expect(4);
       this.calc.Command("1");
       assert.strictEqual(display.text, "1", "Enter 1, display 1");
@@ -138,6 +138,100 @@ QUnit.module("Calculator Input Sequences", function(hooks) {
       this.calc.Command("=");
       assert.strictEqual(display.text, "3", "Enter =, display 3");
     });
+
+    QUnit.test("Subtraction with Decimals", function(assert) {
+      assert.expect(4);
+      this.calc.Command("9");
+      this.calc.Command("9");
+      this.calc.Command(".");
+      this.calc.Command("9");
+      assert.strictEqual(display.text, "99.9", "Enter 99.9, display 99.9");
+      this.calc.Command("-");
+      assert.strictEqual(display.text, "99.9", "Enter -, display 99.9");
+      this.calc.Command("1");
+      this.calc.Command("0");
+      this.calc.Command("0");
+      assert.strictEqual(display.text, "100", "Enter 100, display 100");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "-0.1", "Enter =, display -0.1");
+    });
+
+    QUnit.test("Multiplication with Negatives", function(assert) {
+      assert.expect(5);
+      this.calc.Command("1");
+      this.calc.Command("6");
+      assert.strictEqual(display.text, "16", "Enter 16, display 16");
+      this.calc.Command("+/-");
+      assert.strictEqual(display.text, "-16", "Enter +/-, display -16");
+      this.calc.Command("*");
+      assert.strictEqual(display.text, "-16", "Enter *, display -16");
+      this.calc.Command("2");
+      assert.strictEqual(display.text, "2", "Enter 2, display 2");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "-32", "Enter =, display -32");
+    });
+
+    QUnit.test("Integer Division", function(assert) {
+      assert.expect(4);
+      this.calc.Command("1");
+      this.calc.Command("0");
+      assert.strictEqual(display.text, "10", "Enter 10, display 10");
+      this.calc.Command("/");
+      assert.strictEqual(display.text, "10", "Enter /, display 10");
+      this.calc.Command("5");
+      assert.strictEqual(display.text, "5", "Enter 5, display 5");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "2", "Enter =, display 2");
+    });
+
+    QUnit.test("Division with non-Integer Results", function(assert) {
+      assert.expect(4);
+      this.calc.Command("1");
+      this.calc.Command("0");
+      assert.strictEqual(display.text, "10", "Enter 10, display 10");
+      this.calc.Command("/");
+      assert.strictEqual(display.text, "10", "Enter /, display 10");
+      this.calc.Command("7");
+      assert.strictEqual(display.text, "7", "Enter 7, display 7");
+      this.calc.Command("=");
+      //TODO: VERIFY THIS CALCULATOR RESULT
+      assert.strictEqual(display.text, "1.4285714", "Enter =, display 1.428574");
+    });
+
+    QUnit.test("Division of Decimals", function(assert) {
+      assert.expect(4);
+      this.calc.Command(".");
+      this.calc.Command("6");
+      this.calc.Command("4");
+      assert.strictEqual(display.text, "0.64", "Enter .64, display 0.64");
+      this.calc.Command("/");
+      assert.strictEqual(display.text, "0.64", "Enter /, display 0.64");
+      this.calc.Command(".");
+      this.calc.Command("4");
+      assert.strictEqual(display.text, "0.4", "Enter .4, display 0.4");
+      this.calc.Command("=");
+      assert.strictEqual(display.text, "1.6", "Enter =, display 1.6");
+    });
+
+    QUnit.test("Square Root", function(assert) {
+      assert.expect(2);
+      this.calc.Command("6");
+      this.calc.Command("4");
+      assert.strictEqual(display.text, "64", "Enter 64, display 64");
+      this.calc.Command("sqrt");
+      assert.strictEqual(display.text, "8", "Enter sqrt, display 8");
+    });
+
+    QUnit.test("Square Root of Negative", function(assert) {
+      assert.expect(3);
+      this.calc.Command("3");
+      assert.strictEqual(display.text, "3", "Enter 3, display 3");
+      this.calc.Command("+/-");
+      assert.strictEqual(display.text, "-3", "Enter +/-, display -3");
+      this.calc.Command("sqrt");
+      assert.strictEqual(display.text, "ERR", "Enter sqrt, display ERR");
+    });
+
   });
 
   QUnit.module("Binary Operator Chains", function(hooks) {
